7eca32e846ce57aadad526d42ff8a1ed86de891e
tgline-cms <tgline.cms@gmail.com>
2024-08-11 00:02:54 +0200

    fix config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/static/images`,
+        path: `${__dirname}/static/images/`,
       },
     },
     `gatsby-plugin-image`,
