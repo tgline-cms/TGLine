ac0a51229bb72b1115e99a1352755b0329548bbb
tgline-cms <tgline.cms@gmail.com>
2024-08-11 18:04:59 +0200

    filter gallery images by pavilion id

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,34 +1,23 @@
 import * as React from "react"
 import { Carousel } from "react-bootstrap"
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { useStaticQuery, graphql } from "gatsby"
 import "./slider.scss"
 
 const Slider = ({ product }) => {
-  // Query all files and product galleries
   const data = useStaticQuery(graphql`
     query {
-      allFile {
+      allFile(
+        filter: { relativeDirectory: { regex: "/^pavilion-/" } }
+      ) {
         edges {
           node {
             id
-            base
+            name
+            relativeDirectory
             childImageSharp {
               gatsbyImageData
-            }
-          }
-        }
-      }
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              id
-              exterior_color
-              product_gallery {
-                image
-              }
             }
           }
         }
@@ -36,33 +25,17 @@ const Slider = ({ product }) => {
     }
   `)
 
-  // Find the pavilion with the matching product ID
-  const pavilion = data.allMarkdownRemark.edges.find(
-    edge => edge.node.frontmatter.id === product
-  )
+  const images = data.allFile.edges
+    .filter(({node}) => node.relativeDirectory === `pavilion-${product.toLowerCase()}`)
+    .map(edge => getImage(edge.node.childImageSharp.gatsbyImageData))
 
-
-  if (!pavilion) {
-    return <p>No gallery found for this product.</p>
-  }
-
-  // Extract the product gallery path
-  const gallery = pavilion.node.frontmatter.product_gallery
-  gallery.map((img) => console.log(img.image))
-  // const pavilionName = galleryPath[0].image.split("/")[2]
-
-  // const images = data.allFile.edges.filter(edge =>
-  //   edge.node.base.startsWith(pavilionName)
-  // )
- 
+    console.log(images)
   return (
     <Carousel>
-      {gallery.map(({ image }) => {
-        // const img = getImage(image)
+      {images.map((image) => {
         return (
           <Carousel.Item interval={2000} key={uuidv4()}>
-           {/* { image && (<GatsbyImage image={img} alt={`${pavilion.node.frontmatter.exterior_color}} pavilion`} />)} */}
-           <img src="/images/pavilion-m02/polka.webp" alt={`${pavilion.node.frontmatter.exterior_color} pavilion`} />
+            <GatsbyImage image={image} alt={product} />
 
             <Carousel.Caption>
               <h3>Label for this slide</h3>
