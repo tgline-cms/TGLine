e376a3c8630017c5f1caf9beba4431d75fab02ce
tgline-cms <tgline.cms@gmail.com>
2024-08-13 23:02:13 +0200

    add og-image meta tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,6 +7,7 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { getImage } from 'gatsby-plugin-image'
 
 function Seo({ description, title, children }) {
   const { site } = useStaticQuery(
@@ -19,12 +20,20 @@ function Seo({ description, title, children }) {
             author
           }
         }
+        openGraphDefaultImage: file(
+          relativePath: { eq: "open-graph/code.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(layout: FIXED, height: 580, width: 1200)
+          }
+        }
       }
     `
   )
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
+  const ogImage = getImage(openGraphDefaultImage)
 
   return (
     <>
@@ -33,6 +42,7 @@ function Seo({ description, title, children }) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
+      <meta property="og:image" content={ogImage} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
