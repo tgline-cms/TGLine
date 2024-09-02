cba2f37f4181df0d9d0eb6d6e710254db16f1025
tgline-cms <tgline.cms@gmail.com>
2024-09-02 16:45:30 +0200

    remove comments

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,6 @@ import React from "react"
 import "./src/style/style.scss"
 import Layout from "./src/components/layout"
 
-// Wraps every page in a component
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,6 @@ const IndexPage = () => (
   </>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="TGLine - Pawilony handlowe" />
 
 export default IndexPage
